876a71603f33a9da4e78d96d21da99ffa236c362
Marcucci Ricardo Martín <mmarcucci@ucc.edu.ar>
2019-09-20 13:10:37 -0300

    * Se agrega movimiento al auto * Se agrega bala que dispara con la tecla espacio * Se le da movimiento al a bala

--- main.cpp
+++ main.cpp
@@ -5,14 +5,22 @@ using namespace sf;
 
 int main(int argc, char *argv[]) {
     RenderWindow w(VideoMode(640, 480), "Ejemplo de SFML");
-    Texture t;
-    Sprite s;
+    Texture tAuto, tBala;
+    Sprite sAuto, sBala;
+
+    int xAuto = 100;
+    int yAuto = 130;
+    int xBala = 100;
+    int yBala = 130;
+    bool dibujarBala = false;
 
     w.setFramerateLimit(30);
 
-    t.loadFromFile("auto.png");
-    s.setTexture(t);
-    s.setPosition(100, 130);
+    tAuto.loadFromFile("auto.png");
+    sAuto.setTexture(tAuto);
+    tBala.loadFromFile("bala.png");
+    sBala.setTexture(tBala);
+    sBala.setPosition(50, 50);
 
     while (w.isOpen()) {
         Event e;
@@ -23,10 +31,40 @@ int main(int argc, char *argv[]) {
 
         if (Keyboard::isKeyPressed(Keyboard::Left)) {
             std::cout << "Izq" << std::endl;
+            xAuto += -5;
         }
-        s.setPosition(100, 130);
+        if (Keyboard::isKeyPressed(Keyboard::Right)) {
+            std::cout << "Der" << std::endl;
+            xAuto += 5;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Up)) {
+            std::cout << "Up" << std::endl;
+            yAuto += -5;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Down)) {
+            std::cout << "Down" << std::endl;
+            yAuto += +5;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Space)) {
+            if(!dibujarBala){
+                dibujarBala = true;
+                yBala = yAuto - 16;
+                xBala = xAuto +53/2-7/2;
+            }
+
+        }
+        sAuto.setPosition(xAuto, yAuto);
         w.clear(Color(255, 255, 255, 255));
-        w.draw(s);
+        w.draw(sAuto);
+
+        if (dibujarBala) {
+            sBala.setPosition(xBala, yBala);
+            yBala = yBala - 10;
+            w.draw(sBala);
+            if(yBala < 0)
+                dibujarBala = false;
+        }
+
         w.display();
     }
     return 0;
