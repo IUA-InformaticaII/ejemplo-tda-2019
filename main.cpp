14532c83c9cf451faf636a87d9e9429221aa83a8
Marcucci Ricardo Martín <mmarcucci@ucc.edu.ar>
2019-10-11 15:42:25 -0300

    Fixed Zinjai

--- main.cpp
+++ main.cpp
@@ -5,32 +5,34 @@
 
 using namespace sf;
 
-int main(int argc, char *argv[]) {
+int main() {
+    
+    
     RenderWindow w(VideoMode(640, 480), "Ejemplo de SFML");
     Texture tAuto, tBala;
     Sprite sBala;
-
+    
     // Cargo textura del auto y lo asigno al jugador.
     tAuto.loadFromFile("auto.png");
     Auto player(&tAuto, 640 / 2, 480 / 2);
-
+    
     int xBala = 100;
     int yBala = 130;
     bool dibujarBala = false;
-
-    w.setFramerateLimit(30);
-
+    
+    w.setFramerateLimit(60);
+    
     tBala.loadFromFile("bala.png");
     sBala.setTexture(tBala);
     sBala.setPosition(50, 50);
-
+    
     while (w.isOpen()) {
         Event e;
         while (w.pollEvent(e)) {
             if (e.type == Event::Closed)
                 w.close();
         }
-
+        
         if (Keyboard::isKeyPressed(Keyboard::Left)) {
             player.girar('i');
         }
@@ -43,18 +45,18 @@ int main(int argc, char *argv[]) {
         if (Keyboard::isKeyPressed(Keyboard::Down)) {
             player.frenar();
         }
-
+        
         if (Keyboard::isKeyPressed(Keyboard::Space)) {
             if (!dibujarBala) {
                 dibujarBala = true;
                 yBala = player.getY() - 16;
                 xBala = player.getMedioX() - 7 / 2;
             }
-
+            
         }
         w.clear(Color(255, 255, 255, 255));
         player.dibujar(&w);
-
+        
         if (dibujarBala) {
             sBala.setPosition(xBala, yBala);
             yBala = yBala - 10;
@@ -62,7 +64,7 @@ int main(int argc, char *argv[]) {
             if (yBala < 0)
                 dibujarBala = false;
         }
-
+        
         w.display();
     }
     return 0;
